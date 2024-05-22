c6c30805a7c1a77ab5e092b780fe2c2a6fa17cb6
saulopm <saulopmarinho@terra.com.br>
2024-05-21 21:28:46 -0300

    componetizando a aplicação

---
+++ src/components/Cabecalho.vue
@@ -0,0 +1,13 @@
+<script setup>
+    const  props = defineProps(['tarefasPendentes'])
+</script>
+
+<template>
+    <header class="p-5 mb-4 mt-4 bg-ligth rounded-3">
+    <h1>Minhas Tarefas</h1>
+    <p>
+          Voçe possui {{ props.tarefasPendentes }} tarefas pendentes
+    </p>
+
+  </header>
+</template>
